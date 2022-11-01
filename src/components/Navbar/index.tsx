86db5a75f068896c11f6f2df99e325bbc549ab62
RomuloAlbuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2022-11-01 03:16:07 -0300

    menu hamburguer ok

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,26 +1,48 @@
-import { NavLink } from 'react-router-dom'
-import './styles.css'
+import { NavLink } from "react-router-dom";
+import "./styles.css";
+import "bootstrap/js/src/collapse.js";
 
-const Navbar = ()=>{
-    return (
-        <div className='main-navbar-container'>
-            <div className='content-navbar-container'>
-            <div className='logo-navbar-container'>
-                <a href='link'>Top-Cars</a>
-            </div>
-            <div className='menu-navbar-container'>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='catalogo'>Catálogo</NavLink>
-                    </li>
-                </ul>
-            </div>
-            </div>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm main-navbar-container">
+      <div className="container-fluid content-navbar-container">
+        <div className="logo-navbar-container">
+          <a href="link">
+            <h1>Top-Cars</h1>
+          </a>
         </div>
-    )
-}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mynav"
+          aria-controls="mynav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-export default Navbar
+        <div
+          className="collapse navbar-collapse menu-navbar-container"
+          id="mynav"
+        >
+          <ul className="navbar-nav offset-md-2 list-menu">
+            <li>
+              <NavLink to="/">
+                <h2>Home</h2>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="catalogo">
+                <h2>Catálogo</h2>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
